86cf5f9e14191ab4e608d8c6dbeae29066562141
alyssongoncalves <alyssonwebdev@gmai.com>
2019-05-14 11:14:59 -0300

    Update calculadora.py
    incluindo operação de soma

--- calculadora.py
+++ calculadora.py
@@ -1,1 +1,13 @@
-print ("oi")
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys
+
+def calculadora (operando1, operador, operando2):
+    print (operando1 + operando2)
+	
+#for param in sys.argv :
+#	print (type(param))
+operando1 = int(sys.argv [1])
+operando2 = int(sys.argv [3])
+
+calculadora (operando1, sys.argv [2], operando2) 
