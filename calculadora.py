662aae146164ff3e6b84417e45a43892c9dfb19f
alyssongoncalves <alyssonwebdev@gmai.com>
2019-05-17 14:01:01 -0300

    Update calculadora.py
    Ajustes

--- calculadora.py
+++ calculadora.py
@@ -1,22 +1,28 @@
-while True:
-    operacao = input('Qual operação (+,-,*,/) você quer fazer ou \'exit\' para sair? ')
-    if operacao == 'exit':
-        break
-    elif operacao == '+' or operacao == '-' or operacao == '*' or operacao == '/':
-        operando1 = int(input('Digite o primeiro numero: '))
-        operando2 = int(input('Digite o segundo numero: '))
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys
+
+def calculadora (operando1, operador, operando2):
+    if operador == '+' or operador == '-' or operador == '*' or operador == '/' or operador == '**' or operador == 'mod': 
+        if operador == '+':
+            total = operando1 + operando2
+        elif operador == '-':
+            total = operando1 - operando2
+        elif operador == '*':
+            total = operando1 * operando2
+        elif operador == '/':
+            total = operando1 / operando2
+        elif operador == '**':
+            total = operando1 ** operando2
+        elif operador == 'mod':
+            total = operando1 % operando2
+        print(total)
     else:
-        print('Operacao invalida')
- 
-    if operacao == '+':
-        total = operando1 + operando2
-        print(total)
-    elif operacao == '-':
-        total = operando1 - operando2
-        print(total)
-    elif operacao == '*':
-        total = operando1 * operando2
-        print(total)
-    elif operacao == '/':
-        total = operando1 / operando2
-        print(total)
+        print ("Operacao Invalida!")
+
+#for param in sys.argv :
+#	print (type(param))
+operando1 = int(sys.argv [1])
+operando2 = int(sys.argv [3])
+
+calculadora (operando1, sys.argv [2], operando2) 
