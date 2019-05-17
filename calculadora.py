13203f5a0b52b61d4bfeaff88e1ba68df464e1f7
alyssongoncalves <alyssonwebdev@gmai.com>
2019-05-17 12:38:50 -0300

    Update calculadora.py
    Atualização da operação de soma
    Inclusão  da operação de multiplicação, divisão e subtração
    Inclusão de uma opção de saida

--- calculadora.py
+++ calculadora.py
@@ -1,13 +1,22 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-import sys
-
-def calculadora (operando1, operador, operando2):
-    print (operando1 + operando2)
-	
-#for param in sys.argv :
-#	print (type(param))
-operando1 = int(sys.argv [1])
-operando2 = int(sys.argv [3])
-
-calculadora (operando1, sys.argv [2], operando2) 
+while True:
+    operacao = input('Qual operação (+,-,*,/) você quer fazer ou \'exit\' para sair? ')
+    if operacao == 'exit':
+        break
+    elif operacao == '+' or operacao == '-' or operacao == '*' or operacao == '/':
+        operando1 = int(input('Digite o primeiro numero: '))
+        operando2 = int(input('Digite o segundo numero: '))
+    else:
+        print('Operacao invalida')
+ 
+    if operacao == '+':
+        total = operando1 + operando2
+        print(total)
+    elif operacao == '-':
+        total = operando1 - operando2
+        print(total)
+    elif operacao == '*':
+        total = operando1 * operando2
+        print(total)
+    elif operacao == '/':
+        total = operando1 / operando2
+        print(total)
